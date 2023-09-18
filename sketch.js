efaf6aa407a99259b5b4716476bacf69963c144c
DecoLindo123 <103005716+DecoLindo123@users.noreply.github.com>
2023-09-18 19:40:24 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
-var trex;
+var trex, trex_running, trex_collided;
+var ground, ground_img;
 
 function preload(){
+trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+ground_img = loadAnimation("ground2.png")
 
 }
 function setup(){
 createCanvas(600,200);
 trex = createSprite(50,160,20,50);
-
+trex.addAnimation("running", trex_running);
+trex.scale= 0.5;
+trex.x=50;
+ground = createSprite(200,180,400,20);
+ground.addAnimation("ground2.png",ground_img);
 }
 function draw(){
 background("white");
 drawSprites();
+if(keyDown("space")){
+  trex.velocityY = -10;
+}
 }
